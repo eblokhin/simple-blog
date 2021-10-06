427fdb440808bfb3fe2548d863735a98eb2a795f
Eugene Blokhin <mayson2@yandex.ru>
2021-10-06 18:59:59 +0300

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f712ee4250085633529cf4d5ef50b06d'),
+    },
+  },
+});
